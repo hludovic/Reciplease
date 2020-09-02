af84c5e407ce96fa1c1ed6da825f7d9fec78a303
Ludovic HENRY <ludovic.henry@me.com>
2020-09-02 17:47:40 -0400

    Add a detail view

---
+++ Reciplease/Controller/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Reciplease
+//
+//  Created by Ludovic HENRY on 02/09/2020.
+//  Copyright © 2020 Ludovic HENRY. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageDetail: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ingredientsTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
